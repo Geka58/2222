f0c615dd9e63d95feeb7343f75f0e81bc1c45024
Geka58 <he588@yandex.ru>
2025-04-14 22:22:48 +0300

    реализация Cat в Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
+        Cat cat = new Cat();
         System.out.println("Hello world!!!!");
         System.out.println("Hello world!!!!");
         System.out.println("Hello world!!!!");
         System.out.println("Hello world!!!!");
+        cat.catMessage();
     }
 }
